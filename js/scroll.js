7356f33907897a10cee50470263e75f7b6444a8e
efarfan2393 <e.farfanvaldivia@gmail.com>
2016-10-26 09:56:31 -0500

    subiendo portafolio

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+$(window).bind('scroll', function () {
+  if ($(window).scrollTop() > 668) {
+    $(".menu").addClass('fixed');
+    $(".menu").css("display", "block")
+  } else {
+    $(".menu").removeClass('fixed');
+    $(".menu").css("display", "none");
+  }
+});
