baf9233cfa8b1ebece6bf842e20d13f9a01c594e
efarfan2393 <e.farfanvaldivia@gmail.com>
2016-10-26 19:18:44 -0500

    portafolio

--- js/scroll.js
+++ js/scroll.js
@@ -7,3 +7,14 @@
 //     $(".menu").css("display", "none");
 //   }
 // });
+
+$(".menu").on("click", function(){
+	/*Esto se va a ejecutar cuando alguien
+		haga clic sobre el icono del menu
+
+		addClass para añadir una clase
+		removeClass para remover una clase
+		toggleClass para alternar una clase
+	*/
+	$(".zonaMenu").toggleClass("mostrarMenu");	
+})
